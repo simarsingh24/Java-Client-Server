725ab4f0ba77f6ed23daf014eb2002820aa31d1c
Harsimar <simarsingh24@gmail.com>
2017-05-11 21:26:40 +0530

    initial commit

---
+++ Server.java
@@ -0,0 +1,54 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import java.io.InputStream;
+
+public class Server extends JFrame
+{
+public static void main(String[] args)
+{
+new Server();
+}
+
+
+public Server()
+{
+
+String msg_received;
+try{
+ServerSocket socket = new ServerSocket(8080);
+Socket clientSocket = socket.accept();
+DataInputStream DIS = new DataInputStream(clientSocket.getInputStream());
+msg_received = DIS.readUTF();
+clientSocket.close();
+socket.close();}
+catch(IOException e){msg_received=e.toString();}
+
+if(msg_received==null){
+msg_received="harsimar";
+}
+
+  JTextField t1;  
+    t1=new JTextField(msg_received);  
+    t1.setBounds(50,100, 200,30);  
+	this.add(t1);
+
+JPanel panel1 = new JPanel();
+JLabel label1 = new JLabel("Data Received");
+panel1.add(label1);
+this.add(panel1);
+this.setTitle("Server");
+this.setSize(500,500);
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setVisible(true);
+}
+}
+
+
