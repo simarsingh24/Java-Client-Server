d2a5d4ccb0a7edee1ddf870d3e7e7a6334675497
Harsimar <simarsingh24@gmail.com>
2017-05-11 21:38:19 +0530

    added list support:

--- Server.java
+++ Server.java
@@ -20,7 +20,6 @@ new Server();
 
 public Server()
 {
-
 String msg_received;
 try{
 ServerSocket socket = new ServerSocket(8080);
@@ -35,11 +34,22 @@ if(msg_received==null){
 msg_received="harsimar";
 }
 
+
   JTextField t1;  
     t1=new JTextField(msg_received);  
     t1.setBounds(50,100, 200,30);  
-	this.add(t1);
+this.add(t1);
+	
 
+ DefaultListModel<String> l1 = new DefaultListModel<>();  
+          l1.addElement("Item1");  
+          l1.addElement("Item2");  
+          l1.addElement("Item3");  
+          l1.addElement("Item4");  
+          JList<String> list = new JList<>(l1);  
+          list.setBounds(100,150, 75,75);  
+          this.add(list);  
+          
 JPanel panel1 = new JPanel();
 JLabel label1 = new JLabel("Data Received");
 panel1.add(label1);
@@ -48,6 +58,8 @@ this.setTitle("Server");
 this.setSize(500,500);
 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 this.setVisible(true);
+
+
 }
 }
 
