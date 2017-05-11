e81fc77f8ba0be520cdfbd28c7d0442c3f0eeb9d
Harsimar <simarsingh24@gmail.com>
2017-05-11 21:45:30 +0530

    bug fixes

--- Server.java
+++ Server.java
@@ -20,7 +20,14 @@ new Server();
 
 public Server()
 {
+
 String msg_received;
+DefaultListModel<String> l1 = new DefaultListModel<>();
+JList<String> list = new JList<>(l1);  
+JPanel panel1 = new JPanel();
+JLabel label1 = new JLabel("Data Received");
+panel1.add(label1);
+while(true){          
 try{
 ServerSocket socket = new ServerSocket(8080);
 Socket clientSocket = socket.accept();
@@ -34,33 +41,23 @@ if(msg_received==null){
 msg_received="harsimar";
 }
 
-
+/*
   JTextField t1;  
     t1=new JTextField(msg_received);  
     t1.setBounds(50,100, 200,30);  
 this.add(t1);
-	
-
- DefaultListModel<String> l1 = new DefaultListModel<>();  
-          l1.addElement("Item1");  
-          l1.addElement("Item2");  
-          l1.addElement("Item3");  
-          l1.addElement("Item4");  
-          JList<String> list = new JList<>(l1);  
+*/	
+  
+          l1.addElement(msg_received);  
           list.setBounds(100,150, 75,75);  
           this.add(list);  
           
-JPanel panel1 = new JPanel();
-JLabel label1 = new JLabel("Data Received");
-panel1.add(label1);
 this.add(panel1);
 this.setTitle("Server");
 this.setSize(500,500);
 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 this.setVisible(true);
-
-
 }
 }
-
+}
 
