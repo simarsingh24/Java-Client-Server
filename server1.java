654d35345fcf2c2328dab8169c29688efce20214
Harsimar <simarsingh24@gmail.com>
2017-05-14 10:38:02 +0530

    server wired:

--- server1.java
+++ server1.java
@@ -11,30 +11,58 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 public class server1 extends JFrame{
+	public static DefaultListModel<String> l1 = new DefaultListModel<>();
+	
+	
 	public static void main(String[] args){
 		
 		JFrame frame= new JFrame("Server");
+		
 		JButton fetchButton =new JButton("Fetch");
 		fetchButton.setBounds(20,10,95,30);
+
+		JList<String> list = new JList<>(l1);  
+		list.setBounds(50,50, 200,200);  
 		
-		  final JTextField tf=new JTextField();  
-		    tf.setBounds(50,50, 150,20);  
-		    
+		JPanel panel1 = new JPanel();
+		JLabel label1 = new JLabel("Data Received");
+		panel1.add(label1);
+		
+		frame.add(list);
+		frame.add(panel1);	
 		frame.add(fetchButton);
 		frame.setSize(500,500);
 		frame.setLayout(null);
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		fetchButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
-				tf.setText("Welcome to Javatpoint.");  
-				
-			}
-		});
-	
-		frame.add(tf);
 		
+				fetchData();
+			}
+		});	
 	
+	}
+	
+	public static void fetchData(){
+		String msg_received;
+		
+		try{
+			ServerSocket socket=new ServerSocket(8080);
+			Socket clientSocket=socket.accept();
+			DataInputStream DIS = new DataInputStream(clientSocket.getInputStream());
+			msg_received = DIS.readUTF();
+			clientSocket.close();
+			socket.close();
+		}
+		catch(IOException e){	
+			msg_received=e.toString();
+		}
+		if(msg_received==null){
+			msg_received="harsimar";
+		}
+		l1.addElement(msg_received);
 	}
 }
 
